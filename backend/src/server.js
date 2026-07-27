9c9afa1bad606a41771038ddb6bb31e3ef38bb41
Rohan Singh Negi <rohanwork953@gmail.com>
2026-07-27 10:54:00 +0530

    make some production changes in the backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express"
 import dotenv from "dotenv"
 import { ENV } from "./lib/env.js"
+import path from "path"
 
 
 dotenv.config()
@@ -9,9 +10,15 @@ dotenv.config()
 
 const app = express()
 
+const __direname = path.resolve()
+
 app.get("/health", (req, res) => {
   res.status(200).json({ msg: "api is up and running" });
 });
+
+if(ENV.NODE_ENV === "prodution"){
+  app.use(express.static(path.join(__direname,"../frontend/dist")))
+}
 
 
 app.listen(ENV.PORT, ()=>{
