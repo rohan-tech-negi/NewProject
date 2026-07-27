5e7c5fbf94eb85013c2e18e532d426266ce69d8d
Rohan Singh Negi <rohanwork953@gmail.com>
2026-07-27 11:02:25 +0530

    fixed few issues from the code

--- backend/src/lib/env.js
+++ backend/src/lib/env.js
@@ -1,11 +1,16 @@
 import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
 
-dotenv.config({ quiet: true });
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+dotenv.config({ path: path.resolve(__dirname, "../../.env") });
 
 export const ENV = {
-  PORT: process.env.PORT,
+  PORT: process.env.PORT || 3000,
   DB_URL: process.env.DB_URL,
-  NODE_ENV: process.env.NODE_ENV,
+  NODE_ENV: process.env.NODE_ENV || "development",
   CLIENT_URL: process.env.CLIENT_URL,
   INNGEST_EVENT_KEY: process.env.INNGEST_EVENT_KEY,
   INNGEST_SIGNING_KEY: process.env.INNGEST_SIGNING_KEY,
--- backend/src/server.js
+++ backend/src/server.js
@@ -2,25 +2,27 @@ import express from "express"
 import dotenv from "dotenv"
 import { ENV } from "./lib/env.js"
 import path from "path"
-
+import { fileURLToPath } from "url"
 
 dotenv.config()
 
-
-
 const app = express()
 
-const __direname = path.resolve()
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 app.get("/health", (req, res) => {
   res.status(200).json({ msg: "api is up and running" });
 });
 
-if(ENV.NODE_ENV === "prodution"){
-  app.use(express.static(path.join(__direname,"../frontend/dist")))
+if (ENV.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../../frontend/dist")))
+
+  app.get(/.*/, (req, res) => {
+    res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"))
+  })
 }
 
-
-app.listen(ENV.PORT, ()=>{
-    console.log("port is up on ", ENV.PORT)
+app.listen(ENV.PORT || 3000, ()=>{
+    console.log("port is up on ", ENV.PORT || 3000)
 })
