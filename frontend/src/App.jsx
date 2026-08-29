cf60809ef738a3e6ef2dc9e7fbce6b9bb7dd86b4
Rohan Singh Negi <rohanwork953@gmail.com>
2026-08-29 17:04:18 +0530

    resolved an clerk issue

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,25 @@
 
-import { SignInButton } from '@clerk/react'
+import { SignInButton, SignOutButton, Show, UserButton } from '@clerk/react'
 import './App.css'
 
 function App() {
-
-
   return (
     <>
-     <h1>Welcome to the app</h1>
+       <h1>Welcome to the app</h1>
 
-     <SignInButton></SignInButton>
+      <Show when="signed-out">
+        <SignInButton mode="modal">
+          Sign In
+        </SignInButton>
+      </Show>
+
+      <Show when="signed-in">
+        <SignOutButton>
+          Sign Out
+        </SignOutButton>
+      </Show>
+
+      <UserButton />
     </>
   )
 }
