f86972f4c5e575c29a0c87297b36dd21bac3fe5e
Rohan Singh Negi <rohanwork953@gmail.com>
2026-09-01 23:36:07 +0530

    did some corretions

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,7 +6,7 @@ import { fileURLToPath } from "url";
 import { connectDB } from "./lib/db.js";
 import cors from "cors";
 import {serve} from "inngest/express"
-import { inngest } from "./lib/inngest.js";
+import { inngest , functions} from "./lib/inngest.js";
 dotenv.config();
 
 const app = express();
