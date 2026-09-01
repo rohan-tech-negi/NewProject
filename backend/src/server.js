e6e99e556255e978d595595fc45e8732100c179b
Rohan Singh Negi <rohanwork953@gmail.com>
2026-09-01 23:19:30 +0530

    make some changes in the server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,37 +1,41 @@
-import express from "express"
-import dotenv from "dotenv"
-import { ENV } from "./lib/env.js"
-import path from "path"
-import { fileURLToPath } from "url"
-import { connectDB } from "./lib/db.js"
+import express from "express";
+import dotenv from "dotenv";
+import { ENV } from "./lib/env.js";
+import path from "path";
+import { fileURLToPath } from "url";
+import { connectDB } from "./lib/db.js";
+import cors from "cors";
+dotenv.config();
 
-dotenv.config()
+const app = express();
 
-const app = express()
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
+app.use(express.json());
+app.use(cors({origin: ENV.CLIENT_URL, credentials: true}));
 
 app.get("/health", (req, res) => {
   res.status(200).json({ msg: "api is up and running" });
 });
 
 if (ENV.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../../frontend/dist")))
+  app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 
   app.get(/.*/, (req, res) => {
-    res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"))
-  })
+    res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
+  });
 }
 
 const startServer = async () => {
   try {
     await connectDB();
-    app.listen(ENV.PORT, () => console.log("Server is running on port:", ENV.PORT));
+    app.listen(ENV.PORT, () =>
+      console.log("Server is running on port:", ENV.PORT),
+    );
   } catch (error) {
     console.error("💥 Error starting the server", error);
   }
 };
 
-startServer(); 
-
+startServer();
