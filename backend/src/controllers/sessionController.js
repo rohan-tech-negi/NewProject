2415b51d2af39334aa1e6921a13bc085359d260b
Rohan Singh Negi <rohanwork953@gmail.com>
2026-09-03 14:30:24 +0530

    now be making multiple controllers

---
+++ backend/src/controllers/sessionController.js
@@ -0,0 +1,12 @@
+export async function createSession(req, res) {}
+
+
+export async function getActiveSessions(_, res) {}
+
+export async function getMyRecentSessions(req, res) {}
+
+export async function getSessionById(req, res) {}
+
+export async function joinSession(req, res) {}
+
+export async function endSession(req, res) {}
--- backend/src/routes/sessionRoutes.js
+++ backend/src/routes/sessionRoutes.js
@@ -8,4 +8,9 @@ router.post("/", protectRoute, createSession);
 router.get("/active", protectRoute, getActiveSessions); 
 router.get("/my-recent", protectRoute, getMyRecentSessions);
 
+
+
+router.get("/:id", protectRoute, getSessionById);
+router.post("/:id/join", protectRoute, joinSession);
+router.post("/:id/end", protectRoute, endSession);
 export default router;
