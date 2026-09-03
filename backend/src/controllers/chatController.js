75fcf6fbe71f3b3d7d53c192be1faf4f3b23cddd
Rohan Singh Negi <rohanwork953@gmail.com>
2026-09-03 13:59:05 +0530

    corrected some issues

--- backend/src/controllers/chatController.js
+++ backend/src/controllers/chatController.js
@@ -1,4 +1,4 @@
-import { chatClient } from "../lib/stream";
+import { chatClient } from "../lib/stream.js";
 
 export async function getStreamToken() {
       try {
--- backend/src/routes/chatRoutes.js
+++ backend/src/routes/chatRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
-import { getStreamToken } from "../controllers/chatController";
-import { protectRoute } from "../middleware/protectRoute";
+import { getStreamToken } from "../controllers/chatController.js";
+import { protectRoute } from "../middleware/protectRoute.js";
 
 
 const router = express.Router();
