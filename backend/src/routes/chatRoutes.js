db1d0ce85d84755ce22c3622172a9773ef300b5f
Rohan Singh Negi <rohanwork953@gmail.com>
2026-09-03 13:53:53 +0530

    working on the chatcontroller

--- backend/src/routes/chatRoutes.js
+++ backend/src/routes/chatRoutes.js
@@ -1,7 +1,11 @@
 import express from "express";
+import { getStreamToken } from "../controllers/chatController";
+import { protectRoute } from "../middleware/protectRoute";
 
 
 const router = express.Router();
 
+router.get("/token", protectRoute, getStreamToken);
 
-export default router;
+
+export default router;  
