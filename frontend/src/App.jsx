bbe588bf610b931d96c114ea86fb47713b424595
Rohan Singh Negi <rohanwork953@gmail.com>
2026-09-05 01:04:23 +0530

    done with the work

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,24 @@
 
-import { SignInButton, SignOutButton, Show, UserButton } from '@clerk/react'
+import { SignInButton, SignOutButton, Show, UserButton, useUser } from '@clerk/react'
 import {Routes, Route} from "react-router"
 import HomePage from './pages/HomePage'
 import AboutPage from './pages/AboutPage'
+import ProblemsPage from './pages/ProblemsPage'
+import Toaster from "react-hot-toast"
 
 function App() {
+
+  const {isSignedIn} = useUser()
   return (
     <>
 <Routes>
   <Route path="/" element={<HomePage></HomePage>} />
   <Route path="/about" element={<AboutPage></AboutPage>}></Route>
+  <Route path="/problems" element={<ProblemsPage></ProblemsPage>}></Route>
 </Routes>
 
        
-
+<Toaster/>
       
     </>
   )
