59262637f9c0b933bfd50e47a572aaffd1406ea9
Rohan Singh Negi <rohanwork953@gmail.com>
2026-09-05 00:33:58 +0530

    done the joinsessioncode

--- backend/src/controllers/sessionController.js
+++ backend/src/controllers/sessionController.js
@@ -96,9 +96,35 @@ export async function getSessionById(req, res) {
 
 export async function joinSession(req, res) {
   try {
-    
+    const { id } = req.params;
+    const userId = req.user._id;
+    const clerkId = req.user.clerkId;
+
+    const session = await Session.findById(id);
+
+    if (!session) return res.status(404).json({ message: "Session not found" });
+
+    if (session.status !== "active") {
+      return res.status(400).json({ message: "Cannot join a completed session" });
+    }
+
+    if (session.host.toString() === userId.toString()) {
+      return res.status(400).json({ message: "Host cannot join their own session as participant" });
+    }
+
+    // check if session is already full - has a participant
+    if (session.participant) return res.status(409).json({ message: "Session is full" });
+
+    session.participant = userId;
+    await session.save();
+
+    const channel = chatClient.channel("messaging", session.callId);
+    await channel.addMembers([clerkId]);
+
+    res.status(200).json({ session });
   } catch (error) {
-    
+    console.log("Error in joinSession controller:", error.message);
+    res.status(500).json({ message: "Internal Server Error" });
   }
 }
 
