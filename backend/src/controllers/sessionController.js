8f47e7cea356778ebb1373d94cf50904dfa697e9
Rohan Singh Negi <rohanwork953@gmail.com>
2026-09-05 00:27:47 +0530

    checking the system

---
+++ backend/src/controllers/sessionController.js
@@ -0,0 +1,52 @@
+
+import Session from "../models/Session.js";
+export async function createSession(req, res) {
+    try {
+    const { problem, difficulty } = req.body;
+    const userId = req.user._id;
+    const clerkId = req.user.clerkId;
+
+    if (!problem || !difficulty) {
+      return res.status(400).json({ message: "Problem and difficulty are required" });
+    }
+
+    // generate a unique call id for stream video
+    const callId = `session_${Date.now()}_${Math.random().toString(36).substring(7)}`;
+
+    // create session in db
+    const session = await Session.create({ problem, difficulty, host: userId, callId });
+
+    // create stream video call
+    await streamClient.video.call("default", callId).getOrCreate({
+      data: {
+        created_by_id: clerkId,
+        custom: { problem, difficulty, sessionId: session._id.toString() },
+      },
+    });
+
+    // chat messaging
+    const channel = chatClient.channel("messaging", callId, {
+      name: `${problem} Session`,
+      created_by_id: clerkId,
+      members: [clerkId],
+    });
+
+    await channel.create();
+
+    res.status(201).json({ session });
+  } catch (error) {
+    console.log("Error in create Session controller:", error.message);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+}
+
+
+export async function getActiveSessions(_, res) {}
+
+export async function getMyRecentSessions(req, res) {}
+
+export async function getSessionById(req, res) {}
+
+export async function joinSession(req, res) {}
+
+export async function endSession(req, res) {}
