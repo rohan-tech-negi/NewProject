a04dfd07dd71addbbdba8e287fc337f93629145e
Rohan Singh Negi <rohanwork953@gmail.com>
2026-09-05 00:35:59 +0530

    done the code of the endsession

--- backend/src/controllers/sessionController.js
+++ backend/src/controllers/sessionController.js
@@ -130,8 +130,37 @@ export async function joinSession(req, res) {
 
 export async function endSession(req, res) {
   try {
-    
+     const { id } = req.params;
+    const userId = req.user._id;
+
+    const session = await Session.findById(id);
+
+    if (!session) return res.status(404).json({ message: "Session not found" });
+
+    // check if user is the host
+    if (session.host.toString() !== userId.toString()) {
+      return res.status(403).json({ message: "Only the host can end the session" });
+    }
+
+    // check if session is already completed
+    if (session.status === "completed") {
+      return res.status(400).json({ message: "Session is already completed" });
+    }
+
+    // delete stream video call
+    const call = streamClient.video.call("default", session.callId);
+    await call.delete({ hard: true });
+
+    // delete stream chat channel
+    const channel = chatClient.channel("messaging", session.callId);
+    await channel.delete();
+
+    session.status = "completed";
+    await session.save();
+
+    res.status(200).json({ session, message: "Session ended successfully" });
   } catch (error) {
-    
+      console.log("Error in endSession controller:", error.message);
+    res.status(500).json({ message: "Internal Server Error" });
   }
 }
