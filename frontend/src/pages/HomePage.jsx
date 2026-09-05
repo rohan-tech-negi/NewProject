ba012bb49dcdd60e17d8759e27fa619e84b06f05
Rohan Singh Negi <rohanwork953@gmail.com>
2026-09-06 00:31:26 +0530

    writing the homepage code

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <div className="bg-gradient-to-br from-base-100 via-base-200 to-base-300">
+
+      
+    </div>
   )
 }
 
