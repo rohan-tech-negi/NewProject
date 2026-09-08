05f4c0bf817a46b84dc2f59c10c126a3cc1c3824
Rohan Singh Negi <rohanwork953@gmail.com>
2026-09-08 23:05:46 +0530

    createin first the ui section

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
-
+import { Link, useLocation } from "react-router";
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className="bg-base-100/80 backdrop-blur-md border-b border-primary/20 sticky top-0 z-50 shadow-lg">
+        <div className="max-w-7xl mx-auto p-4 flex items-center justify-between">
+            {/* LOGO */}
+        <Link
+          to="/"
+          className="group flex items-center gap-3 hover:scale-105 transition-transform duration-200"
+        >
+          <div className="size-10 rounded-xl bg-gradient-to-r from-primary via-secondary to-accent flex items-center justify-center shadow-lg ">
+            <SparklesIcon className="size-6 text-white" />
+          </div>
+
+          <div className="flex flex-col">
+            <span className="font-black text-xl bg-gradient-to-r from-primary via-secondary to-accent bg-clip-text text-transparent font-mono tracking-wider">
+              Talent IQ
+            </span>
+            <span className="text-xs text-base-content/60 font-medium -mt-1">Code Together</span>
+          </div>
+        </Link>
+
+        </div>
+    </nav>
   )
 }
 
