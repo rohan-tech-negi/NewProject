34789723ff71deead66aba393bb97f9cfc533ca9
Rohan Singh Negi <rohanwork953@gmail.com>
2026-09-08 23:06:51 +0530

    done with the navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Link, useLocation } from "react-router";
+import { BookOpenIcon, LayoutDashboardIcon, SparklesIcon } from "lucide-react";
+import { UserButton } from "@clerk/clerk-react";
 const Navbar = () => {
   return (
     <nav className="bg-base-100/80 backdrop-blur-md border-b border-primary/20 sticky top-0 z-50 shadow-lg">
@@ -20,6 +22,50 @@ const Navbar = () => {
             <span className="text-xs text-base-content/60 font-medium -mt-1">Code Together</span>
           </div>
         </Link>
+
+        <div className="flex items-center gap-1">
+
+            {/* PROBLEMS PAGE LINK */}
+          <Link
+            to={"/problems"}
+            className={`px-4 py-2.5 rounded-lg transition-all duration-200 
+              ${
+                isActive("/problems")
+                  ? "bg-primary text-primary-content"
+                  : "hover:bg-base-200 text-base-content/70 hover:text-base-content"
+              }
+              
+              `}
+          >
+            <div className="flex items-center gap-x-2.5">
+              <BookOpenIcon className="size-4" />
+              <span className="font-medium hidden sm:inline">Problems</span>
+            </div>
+          </Link>
+
+          {/* DASHBORD PAGE LINK */}
+          <Link
+            to={"/dashboard"}
+            className={`px-4 py-2.5 rounded-lg transition-all duration-200 
+              ${
+                isActive("/dashboard")
+                  ? "bg-primary text-primary-content"
+                  : "hover:bg-base-200 text-base-content/70 hover:text-base-content"
+              }
+              
+              `}
+          >
+            <div className="flex items-center gap-x-2.5">
+              <LayoutDashboardIcon className="size-4" />
+              <span className="font-medium hidden sm:inline">Dashbord</span>
+            </div>
+          </Link>
+
+          <div className="ml-4 mt-2">
+            <UserButton />
+          </div>
+
+        </div>
 
         </div>
     </nav>
