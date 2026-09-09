97837ee8bc1d0d83a378222bb46385f78c2a1d41
Rohan Singh Negi <rohanwork953@gmail.com>
2026-09-09 17:59:07 +0530

    done with the ui part as well

--- frontend/src/pages/ProblemPage.jsx
+++ frontend/src/pages/ProblemPage.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import { useNavigate, useParams } from "react-router";
 import { PROBLEMS } from '../data/problems';
+import Navbar from '../components/Navbar';
+import { Panel, PanelGroup, PanelResizeHandle } from "react-resizable-panels";
 const ProblemPage = () => {
 
   const { id } = useParams();
@@ -33,9 +35,51 @@ const ProblemPage = () => {
 
   const handleRunCode = () =>{}
 
-  
+
   return (
-    <div>ProblemPage</div>
+    <div className="h-screen bg-base-100 flex flex-col">
+      <Navbar></Navbar>
+       <div className="flex-1">
+<PanelGroup direction="horizontal">
+          {/* left panel- problem desc */}
+          <Panel defaultSize={40} minSize={30}>
+            <ProblemDescription
+              problem={currentProblem}
+              currentProblemId={currentProblemId}
+              onProblemChange={handleProblemChange}
+              allProblems={Object.values(PROBLEMS)}
+            />
+          </Panel>
+
+          <PanelResizeHandle className="w-2 bg-base-300 hover:bg-primary transition-colors cursor-col-resize" />
+
+          {/* right panel- code editor & output */}
+          <Panel defaultSize={60} minSize={30}>
+            <PanelGroup direction="vertical">
+              {/* Top panel - Code editor */}
+              <Panel defaultSize={70} minSize={30}>
+                <CodeEditorPanel
+                  selectedLanguage={selectedLanguage}
+                  code={code}
+                  isRunning={isRunning}
+                  onLanguageChange={handleLanguageChange}
+                  onCodeChange={setCode}
+                  onRunCode={handleRunCode}
+                />
+              </Panel>
+
+              <PanelResizeHandle className="h-2 bg-base-300 hover:bg-primary transition-colors cursor-row-resize" />
+
+              {/* Bottom panel - Output Panel*/}
+
+              <Panel defaultSize={30} minSize={30}>
+                <OutputPanel output={output} />
+              </Panel>
+            </PanelGroup>
+          </Panel>
+        </PanelGroup>
+       </div>
+    </div>
   )
 }
 
