bcde8bdbcfbbfd629e801d4fb00d6a1c7ab0b391
Rohan Singh Negi <rohanwork953@gmail.com>
2026-09-09 17:58:00 +0530

    working on the ui part now

--- frontend/src/pages/ProblemPage.jsx
+++ frontend/src/pages/ProblemPage.jsx
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useNavigate, useParams } from "react-router";
+import { PROBLEMS } from '../data/problems';
 const ProblemPage = () => {
 
   const { id } = useParams();
@@ -7,6 +8,32 @@ const ProblemPage = () => {
 
    const [currentProblemId, setCurrentProblemId] = useState("two-sum");
   const [selectedLanguage, setSelectedLanguage] = useState("javascript");
+  const [code, setCode] = useState(PROBLEMS[currentProblemId].starterCode.javascript);
+
+   const [output, setOutput] = useState(null);
+  const [isRunning, setIsRunning] = useState(false);
+
+
+  useEffect(() => {
+    if (id && PROBLEMS[id]) {
+      setCurrentProblemId(id);
+      setCode(PROBLEMS[id].starterCode[selectedLanguage]);
+      setOutput(null);
+    }
+  }, [id, selectedLanguage]);
+  const currentProblem = PROBLEMS[currentProblemId];
+
+  const handleLanguageChange = (e) =>{}
+
+  const handleProblemChange = (e) =>{}
+
+  const triggerConfetti = () => {}
+
+  const checkIfTestsPassed = () =>{}
+
+  const handleRunCode = () =>{}
+
+  
   return (
     <div>ProblemPage</div>
   )
