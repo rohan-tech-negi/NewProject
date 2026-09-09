c74129f2d1b3cee795c4a3f3e7fa7f35ab0f03de
Rohan Singh Negi <rohanwork953@gmail.com>
2026-09-09 17:54:37 +0530

    now be working on the problem page again

--- frontend/src/pages/ProblemPage.jsx
+++ frontend/src/pages/ProblemPage.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
-
+import { useNavigate, useParams } from "react-router";
 const ProblemPage = () => {
+
+  const { id } = useParams();
+  const navigate = useNavigate();
   return (
     <div>ProblemPage</div>
   )
