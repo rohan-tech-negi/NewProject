344842bd293bc182776b0c843f120915eb5d0e26
Rohan Singh Negi <rohanwork953@gmail.com>
2026-09-10 19:38:04 +0530

    making multiple ui parts

--- frontend/src/components/CodeEditorPanel.jsx
+++ frontend/src/components/CodeEditorPanel.jsx
@@ -13,7 +13,34 @@ const CodeEditorPanel = ({
   return (
     <div className="h-full bg-base-300 flex flex-col">
       <div className="flex items-center justify-between px-4 py-3 bg-base-100 border-t border-base-300">
-        
+        <div className="flex items-center gap-3">
+          <img
+            src={LANGUAGE_CONFIG[selectedLanguage].icon}
+            alt={LANGUAGE_CONFIG[selectedLanguage].name}
+            className="size-6"
+          />
+          <select className="select select-sm" value={selectedLanguage} onChange={onLanguageChange}>
+            {Object.entries(LANGUAGE_CONFIG).map(([key, lang]) => (
+              <option key={key} value={key}>
+                {lang.name}
+              </option>
+            ))}
+          </select>
+        </div>
+
+        <button className="btn btn-primary btn-sm gap-2" disabled={isRunning} onClick={onRunCode}>
+          {isRunning ? (
+            <>
+              <Loader2Icon className="size-4 animate-spin" />
+              Running...
+            </>
+          ) : (
+            <>
+              <PlayIcon className="size-4" />
+              Run Code
+            </>
+          )}
+        </button>
       </div>
     </div>
   )
