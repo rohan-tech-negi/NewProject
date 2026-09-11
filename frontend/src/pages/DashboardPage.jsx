fd6c89f55ccc5e83fc04a07727c080a37e090570
Rohan Singh Negi <rohanwork953@gmail.com>
2026-09-11 08:29:38 +0530

    done with the logic part

--- frontend/src/pages/DashboardPage.jsx
+++ frontend/src/pages/DashboardPage.jsx
@@ -16,7 +16,31 @@ const DashboardPage = () => {
   const { data: activeSessionsData, isLoading: loadingActiveSessions } = useActiveSessions();
   const { data: recentSessionsData, isLoading: loadingRecentSessions } = useMyRecentSessions();
 
-  
+  const activeSessions = activeSessionsData?.sessions || [];
+  const recentSessions = recentSessionsData?.sessions || [];
+
+  const handleCreateRoom = () => {
+    if (!roomConfig.problem || !roomConfig.difficulty) return;
+
+    createSessionMutation.mutate(
+      {
+        problem: roomConfig.problem,
+        difficulty: roomConfig.difficulty.toLowerCase(),
+      },
+      {
+        onSuccess: (data) => {
+          setShowCreateModal(false);
+          navigate(`/session/${data.session._id}`);
+        },
+      }
+    );
+  };
+
+   const isUserInSession = (session) => {
+    if (!user.id) return false;
+
+    return session.host?.clerkId === user.id || session.participant?.clerkId === user.id;
+  };
 
 
 
