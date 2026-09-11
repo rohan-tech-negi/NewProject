eb20cf1847c87ea5d380bd5e2eb4b55a96d19e82
Rohan Singh Negi <rohanwork953@gmail.com>
2026-09-11 08:27:53 +0530

    focusing on the logic part first

--- frontend/src/pages/DashboardPage.jsx
+++ frontend/src/pages/DashboardPage.jsx
@@ -1,8 +1,25 @@
 import { useNavigate } from "react-router";
+import { useUser } from "@clerk/clerk-react";
+import { useState } from "react";
+
+import { useActiveSessions, useCreateSession, useMyRecentSessions } from "../hooks/useSessions";
+
 
 const DashboardPage = () => {
 
   const navigate = useNavigate();
+  const { user } = useUser();
+  const [showCreateModal, setShowCreateModal] = useState(false);
+  const [roomConfig, setRoomConfig] = useState({ problem: "", difficulty: "" });
+
+  const createSessionMutation = useCreateSession();
+  const { data: activeSessionsData, isLoading: loadingActiveSessions } = useActiveSessions();
+  const { data: recentSessionsData, isLoading: loadingRecentSessions } = useMyRecentSessions();
+
+  
+
+
+
   return (
     <div>DashboardPage</div>
   )
