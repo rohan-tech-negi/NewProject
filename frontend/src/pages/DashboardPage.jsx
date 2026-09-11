39c6814a3439539755f0280ba68ce43abbd6ad94
Rohan Singh Negi <rohanwork953@gmail.com>
2026-09-11 08:25:28 +0530

    starting with the dashboard page

--- frontend/src/pages/DashboardPage.jsx
+++ frontend/src/pages/DashboardPage.jsx
@@ -1,6 +1,8 @@
-import React from 'react'
+import { useNavigate } from "react-router";
 
 const DashboardPage = () => {
+
+  const navigate = useNavigate();
   return (
     <div>DashboardPage</div>
   )
