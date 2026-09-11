4948e195d1f103d0e6e2f4e3455a648db58da01a
Rohan Singh Negi <rohanwork953@gmail.com>
2026-09-11 08:13:40 +0530

    making every usesession one by one

---
+++ frontend/src/hooks/useSessions.js
@@ -0,0 +1,15 @@
+import { useMutation, useQuery } from "@tanstack/react-query";
+import toast from "react-hot-toast";
+import { sessionApi } from "../api/sessions";
+
+
+export const useCreateSession = () => {
+  const result = useMutation({
+    mutationKey: ["createSession"],
+    mutationFn: sessionApi.createSession,
+    onSuccess: () => toast.success("Session created successfully!"),
+    onError: (error) => toast.error(error.response?.data?.message || "Failed to create room"),
+  });
+
+  return result;
+};
