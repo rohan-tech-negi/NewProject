d792b44307a91db02b2935875eee6d206b69b909
Rohan Singh Negi <rohanwork953@gmail.com>
2026-09-12 18:08:51 +0530

    started the work

--- frontend/src/pages/DashboardPage.jsx
+++ frontend/src/pages/DashboardPage.jsx
@@ -1,5 +1,5 @@
 import { useNavigate } from "react-router";
-import { useUser } from "@clerk/clerk-react";
+import { useUser } from "@clerk/react";
 import { useState } from "react";
 
 import { useActiveSessions, useCreateSession, useMyRecentSessions } from "../hooks/useSessions";
