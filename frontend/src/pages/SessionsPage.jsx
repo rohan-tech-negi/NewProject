cc74e2170e39bc182364ad14b2e6836cc03e5d94
Rohan Singh Negi <rohanwork953@gmail.com>
2026-09-12 19:00:30 +0530

    done with the sessionapage

--- frontend/src/pages/SessionsPage.jsx
+++ frontend/src/pages/SessionsPage.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { useUser } from '@clerk/react';
 import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router";
+import { PROBLEMS } from "../data/problems";
+import { useEndSession, useJoinSession, useSessionById } from '../hooks/useSessions';
 
 const SessionsPage = () => {
    const navigate = useNavigate();
@@ -20,7 +22,23 @@ const SessionsPage = () => {
   const isParticipant = session?.participant?.clerkId === user?.id;
   
 
-  
+    const problemData = session?.problem
+    ? Object.values(PROBLEMS).find((p) => p.title === session.problem)
+    : null;
+
+      const [selectedLanguage, setSelectedLanguage] = useState("javascript");
+  const [code, setCode] = useState(problemData?.starterCode?.[selectedLanguage] || "");
+
+   const handleLanguageChange = (e) => {
+    const newLang = e.target.value;
+    setSelectedLanguage(newLang);
+    // use problem-specific starter code
+    const starterCode = problemData?.starterCode?.[newLang] || "";
+    setCode(starterCode);
+    setOutput(null);
+  };
+
+
   return (
     <div>SessionsPage</div>
   )
