c38c70c2c775b2f090643104dfab37c9fefb3528
Rohan Singh Negi <rohanwork953@gmail.com>
2026-09-12 18:25:05 +0530

    resolved an issue as well

--- backend/src/controllers/sessionController.js
+++ backend/src/controllers/sessionController.js
@@ -1,4 +1,4 @@
-import { chatClient } from "../lib/stream.js";
+import { chatClient, streamClient } from "../lib/stream.js";
 import Session from "../models/Session.js";
 export async function createSession(req, res) {
   try {
