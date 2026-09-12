42a3376c1068537f381f72c78378f4647d4bb528
Rohan Singh Negi <rohanwork953@gmail.com>
2026-09-12 18:56:14 +0530

    working with the sessionpage

--- frontend/src/pages/SessionsPage.jsx
+++ frontend/src/pages/SessionsPage.jsx
@@ -1,6 +1,14 @@
 import React from 'react'
+import { useUser } from '@clerk/react';
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router";
 
 const SessionsPage = () => {
+   const navigate = useNavigate();
+  const { id } = useParams();
+  const { user } = useUser();
+  const [output, setOutput] = useState(null);
+  const [isRunning, setIsRunning] = useState(false);
   return (
     <div>SessionsPage</div>
   )
