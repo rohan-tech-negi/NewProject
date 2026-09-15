d3068e5d268889198a024fcb070c045b6cbdc8ed
Rohan Singh Negi <rohanwork953@gmail.com>
2026-09-15 15:50:36 +0530

    done with the setup

--- frontend/src/pages/SessionsPage.jsx
+++ frontend/src/pages/SessionsPage.jsx
@@ -13,6 +13,7 @@ import CodeEditorPanel from "../components/CodeEditorPanel";
 import OutputPanel from "../components/OutputPanel";
 import useStreamClient from '../hooks/useStreamClient.js';
 import { StreamCall, StreamVideo } from '@stream-io/video-react-sdk';
+import VideoCallUI from '../components/VideoCallUI.jsx';
 
 const SessionsPage = () => {
    const navigate = useNavigate();
