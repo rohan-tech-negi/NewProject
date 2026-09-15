4a242cbdb5e6a0511e415ed84916cbf9092de3cb
Rohan Singh Negi <rohanwork953@gmail.com>
2026-09-15 15:35:42 +0530

    starting with the usestreamclient.js

---
+++ frontend/src/hooks/useStreamClient.js
@@ -0,0 +1,5 @@
+import { useState, useEffect } from "react";
+import { StreamChat } from "stream-chat";
+import toast from "react-hot-toast";
+import { initializeStreamClient, disconnectStreamClient } from "../lib/stream";
+import { sessionApi } from "../api/sessions";  
