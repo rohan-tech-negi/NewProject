277acb438eefaaacfb3df48580f9d499d51d71e5
Rohan Singh Negi <rohanwork953@gmail.com>
2026-09-16 08:21:23 +0530

    wrking on the videocallui

--- frontend/src/components/VideoCallUI.jsx
+++ frontend/src/components/VideoCallUI.jsx
@@ -1,4 +1,15 @@
-import React from 'react'
+import {
+  CallControls,
+  CallingState,
+  SpeakerLayout,
+  useCallStateHooks,
+} from "@stream-io/video-react-sdk";
+import { Loader2Icon, MessageSquareIcon, UsersIcon, XIcon } from "lucide-react";
+import { useState } from "react";
+import { useNavigate } from "react-router";
+import { Channel, Chat, MessageInput, MessageList, Thread, Window } from "stream-chat-react";
+
+/
 
 const VideoCallUI = () => {
   return (
